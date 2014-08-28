2ead6d1756b75a12c6198edc487aa89f2c8c4d6a
Marcus Cooke <marcus.cooke@holidayextras.com>
2014-08-28 16:04:54 +0100

    .

--- logic.js
+++ logic.js
@@ -15,50 +15,101 @@
 //Exclusion Test
 //Data Attributes Test
 
+checkDataAttributes = function( object ){
+	dataCat = $( object ).attr('data-category');
+	dataAct = $( object ).attr('data-action');
+	tagName = self.nodeName.toLowerCase();
+
+	if ( dataCat || dataAct ){
+		if ( dataAct && dataCat ){
+			testAttributes = 1;
+		} else {
+			testAttributes = 0;
+		}
+	} else {
+		if (tags.indexOf(tagName) > -1){
+			testDesiredTag = 0;
+			testAttributes = 0;
+		} else {
+			testDesiredTag = 1;
+			testAttributes = 1;
+		}
+	}
+}
+
+
 var result = {};
 
 var exclusion = [
-    {
-        "type": "class",
-        "element": "selectboxit"
-    },
-    {
-        "type": "id",
-        "element": "selectboxit2"
-    }
+{
+	"type": "class",
+	"element": "selectboxit"
+},
+{
+	"type": "id",
+	"element": "selectboxit2"
+}
 ];
+
+var tags = ["button", "a"];
 
 $.each( $('*'), function(i){
 	self = this;
-	
+	testDesiredTag = 1;
+	testAttributes = 1;
 	$.each( exclusion, function(){
 		switch ( this.type ){
 			case "class":
 				//If we have the excluded class
 				if ( self.className.indexOf( this.element ) > -1 ){
-					exclusionTest = 0;
+					isExcluded = 1;
 				} else {
-					exclusionTest = 1;
+					isExcluded = 0;
 				}
-			break;
-		}
-	});
+				break;
+			}
+		});
+
+	checkDataAttributes(self);
+	testParentAttributes = 0;
+
+	if ( dataTest == 0 ){
+		testParentAttributes = checkDataAttributes( self.parent );
+	}
+
 
 	result[i] = [{
-		"id": "itemID",
+		"id": i,
 		"class": self.className,
-		"exclusionTest": exclusionTest
+		"tagName": tagName,
+		"category": dataCat,
+		"action": dataAct,
+		"isExcluded": isExcluded,
+		"testAttributes": testAttributes,
+		"testParentAttributes": testParentAttributes,
+		"testDesiredTag": testDesiredTag
 	}];
 });
 
+failedResults = {};
+$.each( result, function(j){
+	pDataTest = this[0]['testAttributes'];
+	ptestDesiredTag = this[0]['testDesiredTag'];
+	pParentTest = this[0]['testParentAttributes'];
 
-$.each( result, function(){
-	pass = this[0]['exclusionTest'];
-	if ( pass == 1 ){
-	 console.log( "pass" );
+	failure = 0;
+	if ( pDataTest == 0 || ptestDesiredTag == 0){
+		failure = 1;
+	}
+	if ( this[0]['isExcluded'] == 1 || pParentTest == 1){
+		failure = 0;
+	}
+	if (failure == 1){
+		failedResults[j] = this[0];
 	}
 });
 
+console.log(failedResults);
 
 // Check every A and Button for tracking
 // Data Attr Check
